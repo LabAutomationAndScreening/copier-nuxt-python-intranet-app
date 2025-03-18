f23ed2b0fed2aa905090f47b7e82561b52538f83
Eli Fine <eli88fine@gmail.com>
2025-03-18 14:26:43 -0400

    Basic GraphQL (#6)
    ## Why is this change necessary?
    Basics for GraphQL setup
    
    
     ## How does this change address the issue?
    Adds basic python strawberry setup for GraphQL and Apollo on the
    frontend
    
    
     ## What side effects does this change have?
    None
    
    
     ## How is this change tested?
    Downstream repo

---
+++ template/backend/src/graphql_service/app.py
@@ -0,0 +1,17 @@
+import logging
+from pathlib import Path
+
+import strawberry
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+logger = logging.getLogger(__name__)
+
+
+schema = strawberry.Schema(query=int)
+sdl_path = Path(__file__).parent / "schema.graphql"
+_ = sdl_path.write_text(f"{schema.as_str()}\n")
+graphql_app = GraphQLRouter(schema)
+
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
