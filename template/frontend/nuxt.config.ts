e18f0c4857e0b9297bbef403d04743b7c94c10ec
Eli Fine <eli88fine@gmail.com>
2025-03-18 10:41:36 +0000

    add basic nuxt boilerplate

--- template/frontend/nuxt.config.ts
+++ template/frontend/nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/test-utils", "@nuxt/ui"],
+  css: ["~/assets/css/main.css"],
   experimental: { appManifest: false }, // https://github.com/nuxt/nuxt/issues/30461#issuecomment-2572616714
   vite: {
     // this seems to be explicitly needed when in a devcontainer in order for hot reloading to work
