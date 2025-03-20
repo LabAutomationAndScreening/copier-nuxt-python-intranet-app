1ef7f9a9f2adbc539606aa28dd9be7e8c99645a7
Eli Fine <eli88fine@gmail.com>
2025-03-20 19:29:23 +0000

    CORS

--- template/{% if has_backend %}backend{% endif %}/src/graphql_service/app.py
+++ template/{% if has_backend %}backend{% endif %}/src/graphql_service/app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import strawberry
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 
 logger = logging.getLogger(__name__)
@@ -14,4 +15,11 @@ _ = sdl_path.write_text(f"{schema.as_str()}\n")
 graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Super permissive CORS setting since this is for intranet
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
 app.include_router(graphql_app, prefix="/graphql")
