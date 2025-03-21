36f8327669d8aa6206af4dbb6549f0838e935cda
Eli Fine <eli88fine@gmail.com>
2025-03-21 20:16:04 +0000

    add e2e test

---
+++ template/frontend/tests/e2e/index.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect, describe, beforeAll, afterAll } from "vitest";
+import { setup, $fetch, createPage, url } from "@nuxt/test-utils/e2e";
+import { execSync } from "child_process";
+describe("Index page", async () => {
+  describe("regular nuxt build", async () => {
+    await setup();
+    describe("Static tests of rendered page", async () => {
+      test("component renders Hello world properly", async () => {
+        const html = await $fetch("/");
+        expect(html).toContain("Hello World");
+      });
+    });
+    describe("Full Browser tests", async () => {
+      // TODO: figure out away to display the chromium version so it will show up in logs
+      test("component renders Hello world properly", async () => {
+        const page = await createPage();
+        await page.goto(url("/"), { waitUntil: "hydration" });
+        const text = await page.textContent("div");
+
+        expect(text).toContain("Hello World");
+      });
+    });
+  });
+  describe("docker build", async () => {
+    beforeAll(async () => {
+      console.log("Starting docker-compose...");
+      execSync("docker compose --file=../docker-compose.yaml up --detach --force-recreate --renew-anon-volumes", {
+        stdio: "inherit",
+      });
+      await setup({ host: "http://localhost:3000" });
+    });
+    afterAll(() => {
+      console.log("Stopping docker-compose...");
+      execSync("docker compose --file=../docker-compose.yaml down", { stdio: "inherit" });
+    });
+
+    describe("Full Browser tests", async () => {
+      // TODO: figure out away to display the chromium version so it will show up in logs
+      test("component renders Hello world properly", async () => {
+        const page = await createPage();
+        await page.goto(url("/"), { waitUntil: "hydration" });
+        const text = await page.textContent("div");
+
+        expect(text).toContain("Hello World");
+      });
+    });
+  });
+});
