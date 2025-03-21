e795557b4ad7d7d4a53c11d3b1d2b1d72bf0ab9f
Eli Fine <eli88fine@gmail.com>
2025-03-21 14:31:23 -0400

    Add a job to run vitest for frontend in CI (#16)
    ## Why is this change necessary?
    Good to run CI
    
    
     ## How does this change address the issue?
    Adds job. Also some coverage configuration
    
    
     ## What side effects does this change have?
    None
    
    
     ## How is this change tested?
    Downstream repos w/ and w/o backend
    
     ## Other
    pulled in misc updates from upstream template
    added template question about using GraphQL or not to try and limit
    unneeded code in frontend-only app

--- template/frontend/vitest.config.ts
+++ template/frontend/vitest.config.ts
@@ -1,5 +1,15 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
-  // any custom Vitest config you require
+  test: {
+    sequence: {
+      shuffle: true,
+    },
+    coverage: {
+      reporter: ["text", "json", "html"],
+      reportsDirectory: ".coverage",
+      skipFull: true,
+      ignoreEmptyLines: true,
+    },
+  },
 });
