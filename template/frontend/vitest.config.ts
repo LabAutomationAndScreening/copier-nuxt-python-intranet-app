47a42365936f39dacaf7502dcf70d643c6ef5281
Eli Fine <eli88fine@gmail.com>
2025-03-21 17:36:06 +0000

    shuffle

--- template/frontend/vitest.config.ts
+++ template/frontend/vitest.config.ts
@@ -2,9 +2,14 @@ import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
   test: {
+    sequence: {
+      shuffle: true,
+    },
     coverage: {
       reporter: ["text", "json", "html"],
       reportsDirectory: ".coverage",
+      skipFull: true,
+      ignoreEmptyLines: true,
     },
   },
 });
