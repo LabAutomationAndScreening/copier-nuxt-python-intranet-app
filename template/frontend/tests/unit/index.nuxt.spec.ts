e995fb3effd4b1622d328d1a827874fcf0c3ee7d
Eli Fine <eli88fine@gmail.com>
2025-05-06 20:59:18 +0000

    Descrbe

--- template/frontend/tests/unit/index.nuxt.spec.ts
+++ template/frontend/tests/unit/index.nuxt.spec.ts
@@ -1,5 +1,5 @@
 import { mountSuspended } from "@nuxt/test-utils/runtime";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import Index from "~/pages/index.vue";
 
