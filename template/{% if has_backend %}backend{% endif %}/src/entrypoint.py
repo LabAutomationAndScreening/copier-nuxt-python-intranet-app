091c6dd61e1c32f70bc5084338f5b768a4a8713e
Eli Fine <eli88fine@gmail.com>
2025-05-27 18:06:23 +0000

    whitespace

--- template/{% if has_backend %}backend{% endif %}/src/entrypoint.py
+++ template/{% if has_backend %}backend{% endif %}/src/entrypoint.py
@@ -7,7 +7,6 @@ from backend_api.cli import (
     entrypoint,
 )  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the app
 
-
 _ = app  # pragma: no cover # we can't unit test the entrypoint itself. It is tested in the E2E test of the app
 sys.exit(
     entrypoint(sys.argv[1:])
