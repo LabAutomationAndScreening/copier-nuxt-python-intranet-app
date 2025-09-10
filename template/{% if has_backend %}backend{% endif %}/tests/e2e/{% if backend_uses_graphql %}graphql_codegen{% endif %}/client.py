28ddfb0d2e4556423a887b29e1974c84f9e7d614
Eli Fine <eli88fine@gmail.com>
2025-09-10 16:47:33 +0000

    better stub

--- template/{% if has_backend %}backend{% endif %}/tests/e2e/{% if backend_uses_graphql %}graphql_codegen{% endif %}/client.py
+++ template/{% if has_backend %}backend{% endif %}/tests/e2e/{% if backend_uses_graphql %}graphql_codegen{% endif %}/client.py
@@ -1,2 +1,11 @@
+import httpx
+
+
 class Client:
-    pass
+    def __init__(
+        self,
+        url: str = "",
+        headers: dict[str, str] | None = None,
+        http_client: httpx.Client | None = None,
+    ) -> None:
+        """Stub so that fixture code will not throw import errors, even before full codegen has taken place."""
