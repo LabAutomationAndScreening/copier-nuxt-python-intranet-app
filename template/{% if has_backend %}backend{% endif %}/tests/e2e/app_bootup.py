2118baf63b3b3921f86ac03bac1efca7719bc57b
Eli Fine <eli88fine@gmail.com>
2025-10-14 17:12:04 +0000

    update bootup

--- template/{% if has_backend %}backend{% endif %}/tests/e2e/app_bootup.py
+++ template/{% if has_backend %}backend{% endif %}/tests/e2e/app_bootup.py
@@ -11,6 +11,8 @@ from .jinja_constants import ApplicationBootupModes
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_COMPOSE_FILE = Path(__file__).parent.parent.parent.parent / "docker-compose.yaml"
+
 
 def wait_for_service_to_be_healthy(*, max_retries: int = 15, retry_delay: int = 2, compose_file: str):
     for attempt in range(max_retries):
@@ -57,51 +59,60 @@ def wait_for_service_to_be_healthy(*, max_retries: int = 15, retry_delay: int = 
         raise RuntimeError(f"Application containers failed to become healthy after {max_retries} attempts")
 
 
+def start_compose(*, compose_file: Path = DEFAULT_COMPOSE_FILE):
+    assert compose_file.exists(), f"Compose file {compose_file} does not exist"
+    _ = subprocess.run(  # noqa: S603 # we trust this input
+        [  # noqa: S607 # docker should definitely be in PATH
+            "docker",
+            "compose",
+            "--file",
+            str(compose_file),
+            "build",
+        ],
+        check=True,
+        timeout=300,
+    )
+    _ = subprocess.run(  # noqa: S603 # we trust this input
+        [  # noqa: S607 # docker should definitely be in PATH
+            "docker",
+            "compose",
+            "--file",
+            str(compose_file),
+            "up",
+            "--detach",
+            "--force-recreate",
+            "--renew-anon-volumes",
+            "--remove-orphans",
+        ],
+        check=True,
+        timeout=20,
+    )
+    wait_for_service_to_be_healthy(compose_file=str(compose_file))
+
+
+def stop_compose(*, compose_file: Path = DEFAULT_COMPOSE_FILE):
+    assert compose_file.exists(), f"Compose file {compose_file} does not exist"
+    _ = subprocess.run(  # noqa: S603 # we trust this input
+        [  # noqa: S607 # docker should definitely be in PATH
+            "docker",
+            "compose",
+            "--file",
+            str(compose_file),
+            "down",
+        ],
+        check=True,
+        timeout=45,
+    )
+
+
 @pytest.fixture(scope="session", autouse=True)
 def running_application():
     if APPLICATION_BOOTUP_MODE == ApplicationBootupModes.DOCKER_COMPOSE:
-        compose_file = Path(__file__).parent.parent.parent.parent / "docker-compose.yaml"
-        assert compose_file.exists(), f"Compose file {compose_file} does not exist"
-        _ = subprocess.run(  # noqa: S603 # we trust this input
-            [  # noqa: S607 # docker should definitely be in PATH
-                "docker",
-                "compose",
-                "--file",
-                str(compose_file),
-                "build",
-            ],
-            check=True,
-            timeout=300,
-        )
-        _ = subprocess.run(  # noqa: S603 # we trust this input
-            [  # noqa: S607 # docker should definitely be in PATH
-                "docker",
-                "compose",
-                "--file",
-                str(compose_file),
-                "up",
-                "--detach",
-                "--force-recreate",
-                "--renew-anon-volumes",
-            ],
-            check=True,
-            timeout=20,
-        )
-        wait_for_service_to_be_healthy(compose_file=str(compose_file))
+        start_compose()
     else:
         raise NotImplementedError(f"Unsupported application bootup mode: {APPLICATION_BOOTUP_MODE}")
     yield
     if APPLICATION_BOOTUP_MODE == ApplicationBootupModes.DOCKER_COMPOSE:
-        _ = subprocess.run(  # noqa: S603 # we trust this input
-            [  # noqa: S607 # docker should definitely be in PATH
-                "docker",
-                "compose",
-                "--file",
-                str(compose_file),
-                "down",
-            ],
-            check=True,
-            timeout=45,
-        )
+        stop_compose()
     else:
         raise NotImplementedError(f"Unsupported application bootup mode: {APPLICATION_BOOTUP_MODE}")
