c484e6d09ea573ddb9d75e508db8aba93cc6a5fc
Eli Fine <eli88fine@gmail.com>
2025-10-16 17:08:06 +0000

    conftest

---
+++ template/{% if has_backend %}backend{% endif %}/tests/unit/snapshot.py
@@ -0,0 +1,43 @@
+import json
+from typing import override
+
+import pytest
+from syrupy.assertion import SnapshotAssertion
+from syrupy.extensions.single_file import SingleFileSnapshotExtension
+from syrupy.extensions.single_file import WriteMode
+from syrupy.types import PropertyFilter
+from syrupy.types import PropertyMatcher
+from syrupy.types import SerializableData
+from syrupy.types import SerializedData
+
+
+class SingleTextFileSnapshot(SingleFileSnapshotExtension):
+    _write_mode = (
+        WriteMode.TEXT
+    )  # for some reason the default is binary, but it should be text to make diffs easier to read
+
+
+class SingleFileJsonSnapshot(SingleTextFileSnapshot):
+    file_extension = "json"
+
+    @override
+    def serialize(
+        self,
+        data: SerializableData,
+        *,
+        exclude: PropertyFilter | None = None,
+        include: PropertyFilter | None = None,
+        matcher: PropertyMatcher | None = None,
+    ) -> SerializedData:
+        pretty_data = json.dumps(data, indent=2) + "\n"
+        return super().serialize(
+            pretty_data,
+            exclude=exclude,
+            include=include,
+            matcher=matcher,
+        )
+
+
+@pytest.fixture
+def snapshot_json(snapshot: SnapshotAssertion) -> SnapshotAssertion:
+    return snapshot.use_extension(SingleFileJsonSnapshot)
