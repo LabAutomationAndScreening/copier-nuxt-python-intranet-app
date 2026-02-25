59ba4fdcd667398b313f70dc5cb4651e81aa920c
Eli Fine <eli88fine@gmail.com>
2026-02-25 11:51:29 +0000

    all

--- template/frontend/tests/setup/faker.ts
+++ template/frontend/tests/setup/faker.ts
@@ -1,10 +1,10 @@
 import { faker } from "@faker-js/faker";
-import { beforeEach } from "vitest";
+import { beforeAll } from "vitest";
 
 declare const __TEST_FAKER_SEED__: number;
 
 console.log("[seed passed to faker]", __TEST_FAKER_SEED__);
-beforeEach(() => {
-  // reseed Faker for each test so calls are deterministic
+beforeAll(() => {
+  // ensure faker has specified seed so that test runs could be recreated with the logged seed.
   faker.seed(__TEST_FAKER_SEED__);
 });
