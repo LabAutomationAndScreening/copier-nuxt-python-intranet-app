a6156206bac4b575d2bb5dfb0b54697e06301060
Eli Fine <eli88fine@gmail.com>
2026-02-28 16:04:15 +0000

    poststart

--- .devcontainer/post-start-command.sh
+++ .devcontainer/post-start-command.sh
@@ -4,3 +4,4 @@ set -ex
 # For some reason the directory is not setup correctly and causes build of devcontainer to fail since
 # it doesn't have access to the workspace directory. This can normally be done in post-start-command
 git config --global --add safe.directory /workspaces/copier-nuxt-python-intranet-app
+pre-commit run merge-claude-settings -a
