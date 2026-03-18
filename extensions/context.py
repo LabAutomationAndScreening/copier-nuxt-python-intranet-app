6638f4b21ec90f443bbaa82696e0e3c703f86412
Eli Fine <eli88fine@gmail.com>
2026-03-18 15:35:06 +0000

    More nuxt

--- extensions/context.py
+++ extensions/context.py
@@ -52,7 +52,7 @@ class ContextUpdater(ContextHook):
 
         context["default_node_version"] = "24.11.1"
         context["nuxt_ui_version"] = "^4.5.1"
-        context["nuxt_version"] = "^4.4.2"
+        context["nuxt_version"] = "~4.3.1"
         context["nuxt_icon_version"] = "^2.2.1"
         context["typescript_version"] = "^5.9.3"
         context["playwright_version"] = "^1.58.2"
