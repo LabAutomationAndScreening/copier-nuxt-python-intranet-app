70b87175dec9cb7a6aefeef5149b590949744b9b
Eli Fine <eli88fine@gmail.com>
2026-04-23 17:27:31 +0000

    copier

--- extensions/context.py
+++ extensions/context.py
@@ -106,6 +106,7 @@ class ContextUpdater(ContextHook):
         context["gha_medium_timeout_minutes"] = "8"
         context["gha_long_timeout_minutes"] = "15"
         context["gha_xlong_timeout_minutes"] = "45"
+        context["gha_xxlong_timeout_minutes"] = "90"
 
         context["debian_release_name"] = "trixie"
         context["alpine_image_version"] = "3.23"
