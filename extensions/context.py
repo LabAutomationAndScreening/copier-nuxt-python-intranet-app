05dabfe54886131838b82f530f6ad69c98e77a7b
Eli Fine <eli88fine@gmail.com>
2026-05-11 00:16:28 +0000

    context

--- extensions/context.py
+++ extensions/context.py
@@ -53,8 +53,8 @@ class ContextUpdater(ContextHook):
         context["pytest_asyncio_version"] = ">=1.3.0"
         context["pytest_reserial_version"] = ">=0.6.0"
         context["python_faker_version"] = ">=40.15.0"
-        context["adafruit_circuitpython_busdevice_version"] = "==5.2.14"
-        context["adafruit_circuitpython_register_version"] = "==1.11.1"
+        context["adafruit_circuitpython_busdevice_version"] = "==5.2.17"
+        context["adafruit_circuitpython_register_version"] = "==1.11.3"
 
         context["default_node_version"] = "24.11.1"
         context["nuxt_ui_version"] = "^4.6.0"
