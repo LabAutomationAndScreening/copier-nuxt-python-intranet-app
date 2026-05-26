57b2215616dc9d273df2385c109157bc54124d41
Eli Fine <eli88fine@gmail.com>
2026-05-26 12:42:08 +0000

    entry

--- template/{% if has_backend %}backend{% endif %}/src/entrypoint.py
+++ template/{% if has_backend %}backend{% endif %}/src/entrypoint.py
@@ -5,7 +5,7 @@ pip_system_certs.wrapt_requests.inject_truststore()
 import sys
 
 from backend_api.app_def import app  # noqa: F401 # this needs to be imported for the FastAPI app to actually launch
-from backend_api.cli import entrypoint
+from backend_api.entrypoint.cli import entrypoint
 
 exit_code = entrypoint(sys.argv[1:])
 if (  # needed to enable using hot-reloading with uvicorn. if we always call sys.exit even with 0, then the FastAPI app won't work correctly when launched directly by uvicorn
