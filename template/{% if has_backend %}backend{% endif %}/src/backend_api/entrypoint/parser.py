8373330744960125467ac40c3c6715b31ab74a57
Eli Fine <eli88fine@gmail.com>
2026-06-08 20:34:31 +0000

    get version

--- template/{% if has_backend %}backend{% endif %}/src/backend_api/entrypoint/parser.py
+++ template/{% if has_backend %}backend{% endif %}/src/backend_api/entrypoint/parser.py
@@ -6,12 +6,24 @@ from ..jinja_constants import DEFAULT_DEPLOYED_HOST
 from ..jinja_constants import DEPLOYED_PORT_NUMBER
 
 
-def _get_version() -> str:
-    return f"v{version('backend-api')}"
+# pragma: no mutate start
+def get_version(
+    *,
+    # mutmut isn't picking up the fact that this function gets called when the OpenAPI Schema snapshot gets taken, which causes mutating the default value to be caught
+    prepend_v: bool = False,
+) -> str:
+    # pragma: no mutate end
+    # pragma: no mutate start
+    # This is the name of the package as defined in pyproject.toml # TODO: figure out if there's a way with mutmut to avoid just the case-sensitivity mutations
+    version_str = version("backend-api")
+    # pragma: no mutate end
+    if prepend_v:
+        version_str = f"v{version_str}"
+    return version_str
 
 
 parser = argparse.ArgumentParser(description=APP_NAME, exit_on_error=False)
-_ = parser.add_argument("--version", action="version", version=_get_version())
+_ = parser.add_argument("--version", action="version", version=get_version(prepend_v=True))
 _ = parser.add_argument("--log-level", type=str, default="INFO", help="The log level to use for the logger")
 _ = parser.add_argument("--log-folder", type=str, help="The folder to write logs to")
 _ = parser.add_argument("--port", type=int, default=DEPLOYED_PORT_NUMBER, help="What port to serve the app on")
