ff4b0df4a07c681f71b992fed7706f5ef0565a90
Nathan Zender <github@nathanzender.com>
2026-07-26 08:49:46 -0400

    fix: plural since its an array

--- template/frontend/tests/e2e/helpers/vrt.ts
+++ template/frontend/tests/e2e/helpers/vrt.ts
@@ -20,16 +20,16 @@ export async function expectFullPageScreenshot(
   page: Page,
   name: string,
   {
-    mask = [],
+    masks = [],
     maskLogo = true,
     maskCopyrightYear = true,
     colorSchemes = ["light", "dark"],
-  }: { mask?: Locator[]; maskLogo?: boolean; maskCopyrightYear?: boolean; colorSchemes?: ColorScheme[] } = {},
+  }: { masks?: Locator[]; maskLogo?: boolean; maskCopyrightYear?: boolean; colorSchemes?: ColorScheme[] } = {},
 ): Promise<void> {
   for (const colorScheme of colorSchemes) {
     await page.emulateMedia({ colorScheme });
     await expectFullPageScreenshotInCurrentColorMode(page, withColorSchemeSuffix(name, colorScheme), {
-      mask,
+      masks,
       maskLogo,
       maskCopyrightYear,
     });
@@ -75,10 +75,10 @@ export async function expectFullPageScreenshotInCurrentColorMode(
   page: Page,
   name: string,
   {
-    mask = [],
+    masks = [],
     maskLogo = true,
     maskCopyrightYear = true,
-  }: { mask?: Locator[]; maskLogo?: boolean; maskCopyrightYear?: boolean } = {},
+  }: { masks?: Locator[]; maskLogo?: boolean; maskCopyrightYear?: boolean } = {},
 ): Promise<void> {
   const defaultMasks: Locator[] = [];
   if (maskCopyrightYear) {
@@ -89,7 +89,7 @@ export async function expectFullPageScreenshotInCurrentColorMode(
   }
   await expect(page).toHaveScreenshot(name, {
     fullPage: true,
-    mask: [...defaultMasks, ...mask],
+    mask: [...defaultMasks, ...masks],
   });
 }
 
